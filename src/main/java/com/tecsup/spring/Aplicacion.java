d346c05570c1a24758646d612ea33ba2fabc243f
alumno <alumno@Lab1504-9>
2015-06-28 10:27:30 -0500

    Inicializando Proyecto

---
+++ src/main/java/com/tecsup/spring/Aplicacion.java
@@ -0,0 +1,10 @@
+package com.tecsup.spring;
+public class Aplicacion {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Hola");
+        
+        
+    }
+}
